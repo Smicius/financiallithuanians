d6ec883a3576f49ed41f1a57df5b5dcc349ff2ff
Mantas Smicius <hubismic@gmail.com>
2025-12-24 20:09:34 +0200

    Import fix for production.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import { Imoka2Pakopa } from "/js/parsers/sodra/imoka2Pakopa.js";
-import { XirrCalculator } from "/js/calculators/xirrCalculator.js";
-import { CashFlowEntry } from "/js/cashFlowEntry.js";
-import { Currency } from "/js/currency.js";
+import { Imoka2Pakopa } from "./js/parsers/sodra/imoka2Pakopa.js";
+import { XirrCalculator } from "./js/calculators/xirrCalculator.js";
+import { CashFlowEntry } from "./js/cashFlowEntry.js";
+import { Currency } from "./js/currency.js";
 
 const portfolioValueInput = document.getElementById('currentPortfolio');
 const fileInput = document.getElementById('htmlFile');
